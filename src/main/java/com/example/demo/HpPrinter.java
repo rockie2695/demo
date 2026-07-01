686be32df3f5ca44727381fb07c676a44a3ab891
Aidan <21993381+rockie2695@users.noreply.github.com>
2026-07-02 01:39:50 +0800

    day 9

--- src/main/java/com/example/demo/HpPrinter.java
+++ src/main/java/com/example/demo/HpPrinter.java
@@ -2,11 +2,22 @@ package com.example.demo;
 
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+
 @Component
 public class HpPrinter implements Printer {
 
+    private int count;
+
+    @PostConstruct
+    public void initialize() {
+        count = 5;
+    }
+
     @Override
     public void print(String message) {
+        count--;
         System.out.println("HP印表機: " + message);
+        System.out.println("剩餘使用次數: " + count);
     }
 }

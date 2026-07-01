15280ca4f26cd057e4aac00a01855ccc79339a5f
Aidan <21993381+rockie2695@users.noreply.github.com>
2026-07-01 23:23:42 +0800

    day 7 2

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,8 +9,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RestController
 public class MyController {
 
+    @Autowired
+    public Printer printer;
+
     @RequestMapping("/test")
     public String test() {
+        printer.print("Hello World");
         return "Hello World";
     }
 

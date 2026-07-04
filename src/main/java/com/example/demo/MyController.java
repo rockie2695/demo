7b2b4d17fc5d18f78ee37b0646ed11705030fcb0
Aidan <21993381+rockie2695@users.noreply.github.com>
2026-07-04 21:45:11 +0800

    day 17

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -1,6 +1,10 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,4 +22,13 @@ public class MyController {
         return "Hello World";
     }
 
+    @RequestMapping("/product")
+    public Store product() {
+        Store store = new Store();
+        List<String> list = new ArrayList<>();
+        list.add("蘋果");
+        list.add("橘子");
+        store.setProductList(list);
+        return store;
+    }
 }

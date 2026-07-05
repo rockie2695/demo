2c7b8ea39c5248175c322a27e8c6079ce7105076
Aidan <21993381+rockie2695@users.noreply.github.com>
2026-07-05 20:11:33 +0800

    day27

--- src/main/java/com/example/demo/StudentController.java
+++ src/main/java/com/example/demo/StudentController.java
@@ -1,5 +1,11 @@
 package com.example.demo;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,9 +16,31 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
+
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
     @PostMapping("/students")
     public String create(@RequestBody Student student) {
+        String sql = "INSERT INTO student(id, name) VALUES (:studentId, :studentName)";
+        Map<String, Object> map = new HashMap<>();
+        map.put("studentId", student.getId());
+        map.put("studentName", student.getName());
+        namedParameterJdbcTemplate.update(sql, map);
         return "執行資料庫的 Create 操作";
+    }
+
+    @GetMapping("/students")
+    public List<Student> read() {
+        String sql = "SELECT id, name FROM student";
+
+        Map<String, Object> map = new HashMap<>();
+
+        StudentRowMapper rowMapper = new StudentRowMapper();
+
+        List<Student> list = namedParameterJdbcTemplate.query(sql, map, rowMapper);
+
+        return list;
     }
 
     @GetMapping("/students/{studentId}")
